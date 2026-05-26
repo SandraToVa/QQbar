f4949f0812b344f956f090037781ede1f687529e
Sandra Tomàs <sandratv14.7@gmail.com>
2026-05-26 15:19:19 +0200

    Important update with the full contribution
    Before we only used the full contribution due to 1 term but in here we change all the functions to correctly represent the full contribution includeing all the terms.
    Also the parameteres c are updated. They are not the ones in the paper because those are divided by sqrt(4pi)

--- SandraFiles/Decays/DecayWidth_P1toS.m
+++ SandraFiles/Decays/DecayWidth_P1toS.m
@@ -15,14 +15,15 @@ ComputationIE = TransitionsP1toS('HQp1tS_full');
 
 [I_Cos_cell, I_Sin_cell, I_Si_cell, DeltaE, M] = ComputationIE(2, 2, 0.28, 0.467, 50);
 
-file_name = 'GammaVSmass_HQcharm_1p1-2s_full_bad.txt';
+%%
+
+file_name = 'GammaVSmass_HQcharm_1p1-2s_full56.txt';
 
 % Value of pion mass 140MeV
 m = 0.14;
 % Value of the global constant that divides everithing in GeV. Fpi=92MeV
-FracFpi = 1/(pi^3 * 0.092^4 * 0.187) * 4^2;
+FracFpi = 1/(pi^3 * 0.092^4 * 0.187) ;
 %0.092 és f_pi i 0.187GeV^2 is the string tension
-%4^2 per compensar que Ce=eta/4 i tenim eta^2
 
 %Obtain the I from the cell
 I_Cos_R3 = I_Cos_cell{1, 1}; 
@@ -33,6 +34,7 @@ I_Si_R = I_Si_cell{1, 1};
 I_Si_R2 = I_Si_cell{1, 2};
 I_Si_R3 = I_Si_cell{1, 3};
 I_Si_R4 = I_Si_cell{1, 4};
+I_Si_R0 = I_Si_cell{1, 5};
 
 
 % Filter values where DeltaE > M
@@ -47,56 +49,59 @@ I_Si_R = I_Si_R(valid_idx);
 I_Si_R2 = I_Si_R2(valid_idx);
 I_Si_R3 = I_Si_R3(valid_idx);
 I_Si_R4 = I_Si_R4(valid_idx);
+I_Si_R0 = I_Si_R0(valid_idx);
 
 
 num_valid = length(M_valid);
-DW_values = zeros(1, num_valid);
+DW_values = zeros(4, num_valid);
 
-
-for i = 1:num_valid
-    dwMin1 = P1toSdwM1(I_Cos_R3(i), I_Sin_R2(i), I_Sin_R3(i), I_Sin_R4(i), I_Si_R(i), ...
-    I_Si_R3(i), I_Si_R4(i), m, M_valid(i), DeltaE);
-    
-    dwMin0a = P1toSdwM0_a(I_Cos_R3(i), I_Sin_R3(i), I_Sin_R4(i), I_Si_R(i), I_Si_R2(i),...
-    I_Si_R3(i), I_Si_R4(i), m, M_valid(i), DeltaE);
-
-    dwMin0b = P1toSdwM0_b(I_Cos_R3(i), I_Sin_R3(i), I_Sin_R4(i), I_Si_R(i), I_Si_R2(i),...
-    I_Si_R3(i), I_Si_R4(i), m, M_valid(i), DeltaE);
-
-    dwMin0c = P1toSdwM0_c(I_Cos_R3(i), I_Sin_R3(i), I_Sin_R4(i), I_Si_R(i), I_Si_R2(i),...
-    I_Si_R3(i), I_Si_R4(i), m, M_valid(i), DeltaE);
-
-    dwMin0 = (dwMin0a + dwMin0b + dwMin0c);
-   
-   % Total decay width is the average over initial states Min and addition
-   % to final Min=0. Whe have Min=+1,-1,0 and Jin=1
-    DW_values(i) = ( dwMin1*2 + dwMin0 )/3;
-end
-
-% Now with each part of the gamma separated by parameters we define the
-% parameters computed with mathematica:
-% Each row is a set: [CE]  -> corresponds to [Ce]
-param_sets = [
-    0.0534711;
-    0.115396;
-    -0.115396;
-    -0.0534711
-];
+% We compute the DW_values for each set of lamb, eta, frac:
+% Each row is a set: [lambda, eta, lambda'/2B0] = [lamb, eta, frac]
+param_sets = [  0.0346988,  0.0928852,  -0.44125;  % Red set
+               -0.162547,   0.28906,    -0.439649;   % Green set
+                0.162547,  -0.28906,     0.439649;   % Green set
+               -0.0346988, -0.0928852,   0.44125 ]; % Red set
 
 %Define a decaywidth matrix where each row is a gamma computed with the
 %diferent 4 parameters. If weverything goes well 4 of the results should be
 %the same 2 by 2
 Gamma = zeros(4, num_valid);  % Rows = parameter sets, columns = data points
 
+
 for i = 1:num_valid
-    DW = DW_values(i);
-    
     for N = 1:4
-        CE = param_sets(N);
-        
-        Gamma(N, i) = FracFpi * (CE^2 * DW);
+        lamb = param_sets(N,1);
+        eta = param_sets(N,2);
+        frac = param_sets(N,3);
+
+        %multiplicat per eta^2
+        dwMin1 = P1toSdwM1(I_Cos_R3(i), I_Sin_R2(i), I_Sin_R3(i), I_Sin_R4(i), I_Si_R(i), ...
+        I_Si_R2(i), I_Si_R3(i), I_Si_R4(i), m, M_valid(i), DeltaE, eta);
+    
+        %multiplicat per eta^2
+        dwMin0a = P1toSdwM0_a(I_Cos_R3(i), I_Sin_R2(i), I_Sin_R3(i), I_Sin_R4(i), I_Si_R0(i), I_Si_R(i), I_Si_R2(i),...
+        I_Si_R3(i), I_Si_R4(i), m, M_valid(i), DeltaE, eta);
+    
+        %
+        dwMin0b = P1toSdwM0_b(I_Cos_R3(i), I_Sin_R2(i), I_Sin_R3(i), I_Sin_R4(i), I_Si_R0(i), I_Si_R(i), I_Si_R2(i),...
+        I_Si_R3(i), I_Si_R4(i), m, M_valid(i), DeltaE, lamb, eta, frac);
+
+        dwMin0c = P1toSdwM0_c(I_Cos_R3(i), I_Sin_R2(i), I_Sin_R3(i), I_Sin_R4(i), I_Si_R0(i), I_Si_R(i), I_Si_R2(i),...
+        I_Si_R3(i), I_Si_R4(i), m, M_valid(i), DeltaE, lamb, eta, frac);
+
+        dwMin0 = (dwMin0a + dwMin0b + dwMin0c);
+   
+        % Total decay width is the average over initial states Min and addition
+        % to final Min=0. Whe have Min=+1,-1,0 and Jin=1
+        DW_values(N, i) = ( dwMin1*2 + dwMin0 )/3;
+
+        %Total gamma with constants
+        Gamma(N, i) = FracFpi * DW_values(N, i);
     end
 end
+
+
+
 
 %To cross check we compute also the decay width integrated
 % Compute M^2
@@ -113,15 +118,6 @@ fprintf('Gamma (GeV): %.15f\n', GammaFinal(2));
 fprintf('Gamma (GeV): %.15f\n', GammaFinal(3));
 fprintf('Gamma (GeV): %.15f\n', GammaFinal(4));
 
-% Perform numerical integration for each term
-DW_integral = trapz(M2_valid, DW_values);
-
-GammaIntegral = zeros(1,4);
-for N=1:4
-    CE = param_sets(N);
-
-    GammaIntegral(N) = FracFpi * (CE^2 * DW_integral);
-end
 
 
 
@@ -159,92 +155,109 @@ fprintf('Results saved to %s\n', full_path);
 
 
 
-
+%Corregit afegint tant g5 com g6
 function termM1 = P1toSdwM1(I_Cos_R3, I_Sin_R2, I_Sin_R3, I_Sin_R4, I_Si_R, ...
-    I_Si_R3, I_Si_R4, m, M, DeltaE)
+    I_Si_R2, I_Si_R3, I_Si_R4, m, M, DeltaE, eta)
 
     s=sqrt(DeltaE^2 - M^2);
 
-    constM1 = ( M^2 * sqrt(1 - (4 * m^2) / (M^2))^5 ) / (1280 * s^7 );
+    constM1 = ( M^2 * sqrt(1 - (4 * m^2) / (M^2))^5 * eta^2) / (5120 * s^7 );
 
-    termCos3 = 32 * DeltaE * s * I_Cos_R3;
-    termSin3 = - 4 * pi * s^2 * I_Sin_R3;
-    termSin4 = 8 * DeltaE * (-8 + pi^2) * I_Sin_R4;
-    termSin2 = 8 * DeltaE * s^2 * I_Sin_R2;
-    termSi4 = 2 * pi^3 * DeltaE * I_Si_R4;
-    termSi1 = - s^4 * I_Si_R;
-    termSi3 = - pi^2 * s^2 * I_Si_R3;
+    termCos3 = 48 * DeltaE * s * I_Cos_R3;
+    termSin3 = - 8 * pi * s^2 * I_Sin_R3;
+    termSin4 = 4 * DeltaE * (-24 + 3*pi^2) * I_Sin_R4;
+    termSin2 = 16 * DeltaE * s^2 * I_Sin_R2;
+    termSi4 = 3 * pi^3 * DeltaE * I_Si_R4;
+    termSi1 = - 2*s^4 * I_Si_R;
+    termSi3 = - 2*pi^2 * s^2 * I_Si_R3;
+    termSi2 = pi * DeltaE * s^2 * I_Si_R2;
 
-    termM1 = constM1 * (termCos3 + termSin3 + termSin4 + termSin2 + termSi4 + termSi1 + termSi3)^2;
+    termM1 = constM1 * (termCos3 + termSin3 + termSin4 + termSin2 + termSi4 + termSi1 + termSi3 + termSi2)^2;
 
 end
 
-function termM0_a = P1toSdwM0_a(I_Cos_R3, I_Sin_R3, I_Sin_R4, I_Si_R, I_Si_R2,...
-    I_Si_R3, I_Si_R4, m, M, DeltaE)
+%Corregit afegint tant g5 com g6
+function termM0_a = P1toSdwM0_a(I_Cos_R3, I_Sin_R2, I_Sin_R3, I_Sin_R4, I_Si_R0, I_Si_R, I_Si_R2,...
+    I_Si_R3, I_Si_R4, m, M, DeltaE, eta)
 
     s=sqrt(DeltaE^2 - M^2);
 
-    constM0_a = (3/15) * sqrt(1 - (4 * m^2)/(M^2))^5 * (8*M^4 + 4*M^2*DeltaE^2 + 3*DeltaE^4) ...
-        / (256 * s^11 );
+    constM0_a = sqrt(1 - (4 * m^2)/(M^2))^5 * (8*M^4 + 4*M^2*DeltaE^2 + 3*DeltaE^4) * eta^2 ...
+        / (20480 * pi^2 * s^11 );
+ 
 
-    termA1 = (-32*M^2 + 4*M^2*pi^2 - 64*DeltaE^2 + 8*pi^2*DeltaE^2) * I_Sin_R4;
-    termA2 = pi^3 * (M^2 + 2*DeltaE^2) * I_Si_R4;
-    termA3 = pi * s^2 * (-M^2 - 2*DeltaE^2) * I_Si_R2;
-    termA4 = 16 * s * (2*DeltaE^2 + M^2) * I_Cos_R3;
-    termA5 = 8 * pi * DeltaE * (M^2 - DeltaE^2) * I_Sin_R3;
-    termA6 = 2 * pi^2 * DeltaE * (M^2 - DeltaE^2) * I_Si_R3;
-    termA7 = 2 * DeltaE * s^4 * I_Si_R;
+    termA1 = 48 * pi * s * (2*DeltaE^2 + M^2) * I_Cos_R3;
+    termA2 = 2*pi*(-8+pi^2)*(12*DeltaE^2 + 6*M^2) * I_Sin_R4;
+    termA3 = -32 * pi^2 * s^2 * DeltaE * I_Sin_R3;
+    termA4 = 2 * pi * s^2 * (3*s^2 + M^2 + DeltaE^2) * I_Sin_R2;
+    termA5 = 3*pi^4 * (M^2 + 2*DeltaE^2) * I_Si_R4;
+    termA6 = -8 * pi^3 * DeltaE * s^2 * I_Si_R3;
+    termA7 = -4 * pi^2 * s^2 * (M^2 + DeltaE^2) * I_Si_R2;
+    termA8 = 8 * DeltaE * s^4 * pi * I_Si_R;
+    termA9 = - s^4 * (-M^2 + 2*DeltaE^2) * I_Si_R0;
 
-    termM0_a = constM0_a * (termA1+ termA2 + termA3 + termA4 + termA5 + termA6 + termA7)^2;
+    termM0_a = constM0_a * (termA1+ termA2 + termA3 + termA4 + termA5 + termA6 + termA7 + termA8 + termA9)^2;
 
 end
 
-function termM0_b = P1toSdwM0_b(I_Cos_R3, I_Sin_R3, I_Sin_R4, I_Si_R, I_Si_R2,...
-    I_Si_R3, I_Si_R4, m, M, DeltaE)
+%Corregit afegint tant g5 com g6
+function termM0_b = P1toSdwM0_b(I_Cos_R3, I_Sin_R2, I_Sin_R3, I_Sin_R4, I_Si_R0, I_Si_R, I_Si_R2,...
+    I_Si_R3, I_Si_R4, m, M, DeltaE, lamb, eta, frac)
 
     s=sqrt(DeltaE^2 - M^2);
 
-    constM0_b = 3 * sqrt(1 - (4 * m^2)/(M^2)) / (256 * s^11 );
+    constM0_b = 15 * sqrt(1 - (4 * m^2)/(M^2)) / (20480 * pi^2 * s^11 );
+   
+    termB1 = s^4 * (-8*frac*m^2*s^2 - 2*DeltaE^4*eta - 8*m^2*M^2*(eta+lamb) + ...
+        2*M^4*(eta + 2*lamb) + 4*m^2*DeltaE^2*(3*eta + 2*lamb) - M^2*DeltaE^2*(eta + 4*lamb) ) * I_Si_R0;
+    termB2 = 48 * pi * s * ( 2*M^4 - (12*m^2 + M^2)*DeltaE^2 + 2*DeltaE^4 ) * eta * I_Cos_R3;
+    termB3 = -32 * pi^2 * s^2 * ( -4*m^2 + M^2 + s^2 ) * DeltaE * eta * I_Sin_R3;
+    termB4 = - 8 * pi^3 * s^2 * DeltaE * ( -4*m^2 + DeltaE^2 ) * eta * I_Si_R3;
+    termB5 = 8 * pi * s^4 * DeltaE * (- 4*m^2 + DeltaE^2 ) * eta * I_Si_R;
+    termB6 = 12 * pi * (-8 + pi^2) * (2*s^4 + 3 * (-4*m^2 + M^2)*DeltaE^2 ) * eta * I_Sin_R4;
+    termB7 = 3 * pi^4 * ( 2*M^4 - ( 12*m^2 + M^2) * DeltaE^2 + 2*DeltaE^4 ) * eta * I_Si_R4;
+    termB8 = 4 * pi* s^2 * (8*frac*m^2*s^2 + 2*M^2*s^2*eta + M^2*DeltaE^2*eta + 2*s^2*DeltaE^2*eta + ...
+        4*M^2*s^2*lamb - 4*m^2*(DeltaE^2*eta + 2*s^2*(eta + lamb) ) ) * I_Sin_R2;
+    termB9 = 4* pi^2 * s^2 * ( 2*frac*m^2*s^2 + ( -2*M^4 + 2*m^2*(M^2 + 3*DeltaE^2) + M^2*DeltaE^2 - DeltaE^4)*eta - ...
+        s^2*(2*m^2 - M^2)*lamb ) * I_Si_R2;
 
-    termB1 = ( 8*M^4*(-8+pi^2) + 384*m^2*DeltaE^2 + 32*M^2*DeltaE^2 - 48*m^2*pi^2*DeltaE^2 - ...
-        4*M^2*pi^2*DeltaE^2 - 64*DeltaE^4 + 8*pi^2*DeltaE^4 ) * I_Sin_R4;
-    termB2 = pi^3 * ( 2*M^4 -12*m^2*DeltaE^2 - M^2*DeltaE^2 + 2*DeltaE^4 ) * I_Si_R4;
-    termB3 = pi * s^2 * ( -2*M^4 +12*m^2*DeltaE^2 + M^2*DeltaE^2 - 2*DeltaE^4) * I_Si_R2;
-    termB4 = 16 * s * ( 2*M^4 - (12*m^2+M^2)*DeltaE^2 + 2*DeltaE^4 ) * I_Cos_R3;
-    termB5 = 8 * pi * DeltaE * ( -4*m^2*M^2 + 4*m^2*DeltaE^2 + M^2*DeltaE^2 - DeltaE^4 ) * I_Sin_R3;
-    termB6 = 2 * pi^2 * DeltaE * ( -4*m^2*M^2 + 4*m^2*DeltaE^2 + M^2*DeltaE^2 - DeltaE^4 ) * I_Si_R3;
-    termB7 = 2 * DeltaE * s^2 * ( 4*m^2*M^2 - 4*m^2*DeltaE^2 - M^2*DeltaE^2 + DeltaE^4 ) * I_Si_R;
-
-    termM0_b = constM0_b * (termB1+ termB2 + termB3 + termB4 + termB5 + termB6 + termB7)^2;
+    termM0_b = constM0_b * (termB1+ termB2 + termB3 + termB4 + termB5 + termB6 + termB7 + termB8 + termB9)^2;
 
 end
 
-function termM0_c = P1toSdwM0_c(I_Cos_R3, I_Sin_R3, I_Sin_R4, I_Si_R, I_Si_R2,...
-    I_Si_R3, I_Si_R4, m, M, DeltaE)
+%Corregit afegint tant g5 com g6
+function termM0_c = P1toSdwM0_c(I_Cos_R3, I_Sin_R2, I_Sin_R3, I_Sin_R4, I_Si_R0, I_Si_R, I_Si_R2,...
+    I_Si_R3, I_Si_R4, m, M, DeltaE, lamb, eta, frac)
 
     s=sqrt(DeltaE^2 - M^2);
 
-    constM0_c = 2 * sqrt(1 - (4 * m^2)/(M^2))^3 * (2*M^2 + DeltaE^2) ...
-        / (256 * s^11 );
+    constM0_c = - 10 * sqrt(1 - (4 * m^2)/(M^2))^3 * (2*M^2 + DeltaE^2) * eta ...
+        / (20480 * pi^2 * s^11 );
+  
+    termA1 = 48 * pi * s * (2*DeltaE^2 + M^2) * I_Cos_R3;
+    termA2 = 2*pi*(-8+pi^2)*(12*DeltaE^2 + 6*M^2) * I_Sin_R4;
+    termA3 = -32 * pi^2 * s^2 * DeltaE * I_Sin_R3;
+    termA4 = 2 * pi * s^2 * (3*s^2 + M^2 + DeltaE^2) * I_Sin_R2;
+    termA5 = 3*pi^4 * (M^2 + 2*DeltaE^2) * I_Si_R4;
+    termA6 = -8 * pi^3 * DeltaE * s^2 * I_Si_R3;
+    termA7 = -4 * pi^2 * s^2 * (M^2 + DeltaE^2) * I_Si_R2;
+    termA8 = 8 * DeltaE * s^4 * pi * I_Si_R;
+    termA9 = - s^4 * (-M^2 + 2*DeltaE^2) * I_Si_R0;
+    termB1 = s^4 * (-8*frac*m^2*s^2 - 2*DeltaE^4*eta - 8*m^2*M^2*(eta+lamb) + ...
+        2*M^4*(eta + 2*lamb) + 4*m^2*DeltaE^2*(3*eta + 2*lamb) - M^2*DeltaE^2*(eta + 4*lamb) )*I_Si_R0;
+    termB2 = 48 * pi * s * ( 2*M^4 - (12*m^2+M^2)*DeltaE^2 + 2*DeltaE^4 ) * eta * I_Cos_R3;
+    termB3 = -32 * pi^2 * s^2 * ( -4*m^2 + M^2 + s^2 ) * DeltaE * eta * I_Sin_R3;
+    termB4 = -8 * pi^3 * s^2 * DeltaE * ( -4*m^2 + DeltaE^2 ) * eta * I_Si_R3;
+    termB5 = 8 * pi * s^4 * DeltaE * (- 4*m^2 + DeltaE^2 ) * eta * I_Si_R;
+    termB6 = 12 * pi * (-8 + pi^2) * (2*s^4 + 3 * (-4*m^2 + M^2)*DeltaE^2 ) * eta * I_Sin_R4;
+    termB7 = 3 * pi^4 * ( 2*M^4 - ( 12*m^2 + M^2) * DeltaE^2 + 2*DeltaE^4 ) * eta * I_Si_R4;
+    termB8 = 4 * pi* s^2 * (8*frac*m^2*s^2 + 2*M^2*s^2*eta + M^2*DeltaE^2*eta + 2*s^2*DeltaE^2*eta + ...
+        4*M^2*s^2*lamb - 4*m^2*(DeltaE^2*eta + 2*s^2*(eta + lamb) ) ) * I_Sin_R2;
+    termB9 = 4* pi^2 * s^2 * ( 2*frac*m^2*s^2 + ( -2*M^4 + 2*m^2*(M^2 + 3*DeltaE^2) + M^2*DeltaE^2 - DeltaE^4)*eta - ...
+        s^2*(2*m^2 - M^2)*lamb ) * I_Si_R2;
 
-    termA1 = (-32*M^2 + 4*M^2*pi^2 - 64*DeltaE^2 + 8*pi^2*DeltaE^2) * I_Sin_R4;
-    termA2 = pi^3 * (M^2 + 2*DeltaE^2) * I_Si_R4;
-    termA3 = pi * s^2 * (-M^2 - 2*DeltaE^2) * I_Si_R2;
-    termA4 = 16 * s * (2*DeltaE^2 + M^2) * I_Cos_R3;
-    termA5 = 8 * pi * DeltaE * (M^2 - DeltaE^2) * I_Sin_R3;
-    termA6 = 2 * pi^2 * DeltaE * (M^2 - DeltaE^2) * I_Si_R3;
-    termA7 = 2 * DeltaE * s^4 * I_Si_R;
-    termB1 = ( 8*M^4*(-8+pi^2) + 384*m^2*DeltaE^2 + 32*M^2*DeltaE^2 - 48*m^2*pi^2*DeltaE^2 - ...
-        4*M^2*pi^2*DeltaE^2 - 64*DeltaE^4 + 8*pi^2*DeltaE^4 ) * I_Sin_R4;
-    termB2 = pi^3 * ( 2*M^4 -12*m^2*DeltaE^2 - M^2*DeltaE^2 + 2*DeltaE^4 ) * I_Si_R4;
-    termB3 = pi * s^2 * ( -2*M^4 +12*m^2*DeltaE^2 + M^2*DeltaE^2 - 2*DeltaE^4) * I_Si_R2;
-    termB4 = 16 * s * ( 2*M^4 - (12*m^2+M^2)*DeltaE^2 + 2*DeltaE^4 ) * I_Cos_R3;
-    termB5 = 8 * pi * DeltaE * ( -4*m^2*M^2 + 4*m^2*DeltaE^2 + M^2*DeltaE^2 - DeltaE^4 ) * I_Sin_R3;
-    termB6 = 2 * pi^2 * DeltaE * ( -4*m^2*M^2 + 4*m^2*DeltaE^2 + M^2*DeltaE^2 - DeltaE^4 ) * I_Si_R3;
-    termB7 = 2 * DeltaE * s^2 * ( 4*m^2*M^2 - 4*m^2*DeltaE^2 - M^2*DeltaE^2 + DeltaE^4 ) * I_Si_R;
-
-    termM0_c = constM0_c * (termA1+ termA2 + termA3 + termA4 + termA5 + termA6 + termA7) * ...
-        (-termB1 - termB2 - termB3 - termB4 - termB5 - termB6 - termB7);
+    termM0_c = constM0_c * (termA1+ termA2 + termA3 + termA4 + termA5 + termA6 + termA7 + termA8 + termA9) * ...
+        (termB1 + termB2 + termB3 + termB4 + termB5 + termB6 + termB7 + termB8 + termB9);
 
 end
 
